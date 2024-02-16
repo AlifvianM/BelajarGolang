522e8ea3c1bdfbde6aa10604bc4389e242d5d34d
AlifvianMarco <alifvianm@gmail.com>
2024-02-17 05:55:34 +0700

    adding fizzbuzz in latihan

--- Latihan/fizzbuzz.go
+++ Latihan/fizzbuzz.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import "fmt"
 
+func fizzbuzz(count int) {
+	for i := 1; i < count; i++ {
+		//print fizzbuzz
+		if (i%3 == 0) && (i%5 == 0) {
+			fmt.Println("FizzBuzz =", i)
+		}
+
+		if i%3 == 0 {
+			fmt.Println("Fizz =", i)
+		}
+
+		if i%5 == 0 {
+			fmt.Println("Buzz =", i)
+		} else {
+			fmt.Println(i)
+		}
+
+	}
+}
+
+func main() {
+	fizzbuzz(30)
 }
